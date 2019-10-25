a3b4d8aef9e77e3e577609a6ff58c382c42cc795
Sumit Chauhan <sumitchauhanrke@gmail.com>
2019-10-25 13:03:39 +0530

    Fixed all errors

--- routes/csv.js
+++ routes/csv.js
@@ -1,21 +1,9 @@
 const router = require('express').Router()
 const csv = require('csv-parser')
-// const createCsvWriter = require('csv-writer').createObjectCsvWriter
 const fs = require('fs')
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const stringify = require('csv-stringify')
-
-// const path = require('path')
-// const csvWriter = createCsvWriter({
-//   path: 'sample.csv',
-//   header: [
-//     { id: 'first', title: 'First' },
-//     { id: 'last', title: 'Last' },
-//     { id: 'month', title: 'Month' },
-//     { id: 'day', title: 'Day' }
-//   ]
-// })
 
 router.post('/', upload.single('file'), (req, res) => {
   const data = []
@@ -28,27 +16,13 @@ router.post('/', upload.single('file'), (req, res) => {
       data.forEach(element => {
         delete element[' Unused']
       })
-      //   console.log(data)
       res.setHeader('Content-Type', 'text/csv')
       res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"')
       res.setHeader('Cache-Control', 'no-cache')
       res.setHeader('Pragma', 'no-cache')
       stringify(data, { header: true })
         .pipe(res)
-    //   csvWriter
-    //     .writeRecords(data)
-    //     .then(() => {
-    //       console.log('The CSV file was written successfully')
-    //       var parentDirectory = path.join(__dirname, '/../')
-    //       var filepath = path.join(parentDirectory, 'sample.csv')
-    //       var stat = fs.statSync(filepath)
-    //       res.writeHead(200, { 'Content-Type': 'text/csv', 'Content-Length': stat.size })
-    //       var readStream = fs.createReadStream(filepath)
-    //       readStream.pipe(res)
-    //     })
-    //     .catch(err => res.json(err))
     })
-//   return res.json({ message: 'in the route' })
 })
 
 module.exports = router
